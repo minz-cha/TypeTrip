dbe53806c6a924063eb4a14d5b2756ae57243ccf
JongMin Kim <qkfka108@naver.com>
2023-06-29 14:41:05 +0900

    ttt

--- deploy-release.sh
+++ deploy-release.sh
@@ -3,6 +3,6 @@
 mkdir ./build
 cp -r ~/typetrip/react/build ./
 
-docker compose -f release/docker-compose.yml build --no-cache
-docker compose -f release/docker-compose.yml down
-docker compose -f release/docker-compose.yml up -d
+docker compose -f ./release/docker-compose.yml build --no-cache
+docker compose -f ./release/docker-compose.yml down
+docker compose -f ./release/docker-compose.yml up -d
